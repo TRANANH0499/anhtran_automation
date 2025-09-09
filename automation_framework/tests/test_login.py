27cefa109e7dd9ef227a839563e6febcd1f60144
TranAnh99 <tthoanganhvn@gmail.com>
2025-09-09 21:03:44 +0700

    tests auto

--- automation_framework/tests/test_login.py
+++ automation_framework/tests/test_login.py
@@ -1,23 +1,21 @@
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
+from base.base_test import BaseTest as BaseTest
+from selenium.webdriver.support.ui import WebDriverWait 
 from time import sleep
-
-def test_login():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    sleep(5)
-
-    username = driver.find_element(By.XPATH, "//input[@name='username']")
-    password = driver.find_element(By.XPATH, "//input[@name='password']")
-    login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-
-    username.send_keys("Admin")
-    password.send_keys("admin123")
-    login_button.click()
-
-    sleep(10)
-    driver.quit()
-
-test_login()
-
+class TestLogin(BaseTest):
+    
+    USERNAME_INPUT = (By.NAME,"username")
+    PASSWORD_INPUT = (By.NAME, "password")
+    LOGIN_INPUT    = (By.XPATH, "//button[@type='submit']")
+    
+    def test_login(self):
+        username = self.driver.find_element(*self.USERNAME_INPUT)
+        password = self.driver.find_element(*self.PASSWORD_INPUT)
+        username.send_keys('Admin')
+        password.send_keys('admin123')        
+        btnlogin = self.driver.find_element(*self.LOGIN_INPUT)
+            
+        btnlogin.click()
+        sleep(7)  # import time
+        

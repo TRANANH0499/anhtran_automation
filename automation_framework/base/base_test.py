54d526e7a6e389374413e576226f54d37737aa94
ANHTTH <tthoanganhvn@gmail.com>
2025-09-20 19:36:15 +0700

    ciyml

--- automation_framework/base/base_test.py
+++ automation_framework/base/base_test.py
@@ -1,17 +1,22 @@
 import pytest
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.by import By
 
 
 class BaseTest:
     @pytest.fixture(scope= "class", autouse=True)
     
     def setup(self, request):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless.new")
+        
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")   
         sleep(5)
         request.cls.driver = self.driver
+        
         
         yield
         self.driver.quit()
